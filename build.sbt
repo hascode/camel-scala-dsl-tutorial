b84cdbdba764f3eb98591f55f420b0e90264bf81
hascode <hascode@localhost>
2013-01-11 08:20:12 +0100

    IMAP route example added.

--- build.sbt
+++ build.sbt
@@ -13,3 +13,9 @@ libraryDependencies += "org.apache.camel" % "camel-jms" % "2.10.1"
 libraryDependencies += "ch.qos.logback" % "logback-core" % "1.0.9"
 
 libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.0.9"
+
+libraryDependencies += "javax.mail" % "mail" % "1.4.5"
+
+libraryDependencies += "com.icegreen" % "greenmail" % "1.3"
+
+libraryDependencies += "org.apache.camel" % "camel-mail" % "2.10.3"
--- src/main/scala/com/hascode/tutorial/ImapExample.scala
+++ src/main/scala/com/hascode/tutorial/ImapExample.scala
@@ -1,17 +1,39 @@
 package com.hascode.tutorial
-import org.apache.camel.scala.dsl.builder.RouteBuilder
-import org.apache.camel.CamelContext
 import org.apache.camel.impl.DefaultCamelContext
+import org.apache.camel.scala.dsl.builder.RouteBuilder
 import org.apache.camel.scala.dsl.builder.RouteBuilderSupport
+import org.apache.camel.CamelContext
+import com.icegreen.greenmail.user.GreenMailUser
+import com.icegreen.greenmail.util.GreenMail
+import com.icegreen.greenmail.util.ServerSetupTest
+import javax.mail.internet.MimeMessage
+import javax.mail.Session
+import javax.mail.internet.InternetAddress
+import javax.mail.Message
 
 object ImapExample extends App {
+  val mailServer = new GreenMail(ServerSetupTest.IMAP)
+  mailServer.start
+  val user = mailServer.setUser("test@hascode.com", "joe", "XXXX");
+
   val context: CamelContext = new DefaultCamelContext
   val routeBuilder = new RouteBuilder {
-    //TODO: use/bootstrap greenmail server
-    from("imap://hostname[:port]") --> ("file:data/outbox");
+    from("imap://joe@0.0.0.0:3143?password=XXXX") --> ("file:data/outbox");
   }
   context.addRoutes(routeBuilder)
   context.start
-  while (true) {}
+  Thread.sleep(3000)
+  for (i <- 1 until 3) {
+    var message = new MimeMessage(null: Session);
+    message.setFrom(new InternetAddress("test@hascode.com"));
+    message.addRecipient(Message.RecipientType.TO, new InternetAddress(
+      "foo@hascode.com"));
+    message.setSubject("Test E-Mail #" + i);
+    message.setText("This is a fine test e-mail. It is the " + i + " message of 3.");
+    user.deliver(message);
+    Thread.sleep(2000)
+  }
+
+  mailServer.stop
 }
 

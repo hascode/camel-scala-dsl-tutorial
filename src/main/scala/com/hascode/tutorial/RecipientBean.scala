3df14212542d29958b8a66ce407cac4a44c74491
hascode <hascode@localhost>
2013-02-11 21:40:41 +0100

    Java artifacts removed.

--- src/main/scala/com/hascode/tutorial/RecipientBean.scala
+++ src/main/scala/com/hascode/tutorial/RecipientBean.scala
@@ -6,9 +6,8 @@ class RecipientsBean {
 
   @RecipientList def getRecipients(@Header("CamelFileName") fileName: String): Array[String] = {
     println("detecting recipients for given filename: " + fileName)
-    if (fileName.endsWith("csv")) {
-      return Array("jms:csvQueue")
-    }
-    return Array("jms:defaultQueue")
+    if (fileName.endsWith("csv"))
+      Array("jms:csvQueue")
+    Array("jms:defaultQueue")
   }
 }
